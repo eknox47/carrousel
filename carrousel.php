151994b35afe9728e2f02a3b1aa19cd3fb769074
eknox47 <edaigneault47@gmail.com>
2023-03-08 10:51:26 -0500

    c11 debut du carrousel

---
+++ carrousel.php
@@ -0,0 +1,18 @@
+<?php 
+
+/**
+ * Extension carrousel permet d'afficher dans une boite modale 
+ * les images d'une galerie
+ * Package name : Carrousel
+ * Version: 1.0.0
+ */
+
+/*
+Plugin Name: Carrousel
+Author: Émile Daigneault
+Plugin URI: https:github.com/eknox47/carrousel
+Description: Permet d'afficher dans une boite modale les images d'une galerie avec un systême de navigation
+
+*/
+
+?>
---
+++ js/carrousel.js
@@ -0,0 +1,5 @@
+(function(){
+//Fonction IFEE
+console.log("debut du carrousel");
+
+})()
