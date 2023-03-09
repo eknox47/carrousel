45cb97c27cf69bf32a5e8ea403a1ac573416e56d
eknox47 <edaigneault47@gmail.com>
2023-03-09 15:22:00 -0500

    c12 affichage et animation de la boite modale

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,5 +1,20 @@
 (function(){
 //Fonction IFEE
-console.log("debut du carrousel");
+    console.log("debut du carrousel");
 
+    let bouton__ouvir = document.querySelector(".bouton__ouvrir");
+    let elemCarrousel = document.querySelector(".carrousel");
+    let elemBouton__x = document.querySelector(".bouton__x");
+
+    bouton__ouvir.addEventListener("mousedown", function(){
+
+        elemCarrousel.classList.add("carrousel--ouvrir");
+
+    })
+
+    elemBouton__x.addEventListener("mousedown", function(){
+
+        elemCarrousel.classList.remove("carrousel--ouvrir");
+
+    })
 })()
